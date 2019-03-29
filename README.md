3dd3f53a4bdf8edba4f017efb02d79e86f8e76c0
Aarsh Sharma <aarsh01rsh@gmail.com>
2019-03-29 23:11:29 +0530

    login implemented

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Articulus
 
 Write and read articles.
+
+## Keys
+
+Add keys in "./config/keys.js" and export them
+
+> mongoURI: `"mongodb://<dbuser>:<dbpassword>@ds127646.mlab.com:27646/articulus"`,
+>
+> secretOrkey: "secretkey"
--- routes/api/users.js
+++ routes/api/users.js
@@ -10,14 +10,17 @@ const passport = require("passport");
 // const validateRegisterInput = require("../../validation/register");
 // const validateLoginInput = require("../../validation/login");
 
-//Load User model
+// Load User model
 const User = require("../../models/User");
+
+// import secret key
+const secretOrKey = require("../../config/keys").secretOrkey;
 
 // @route   POST api/users/register
 // @desc    Register a user
 // @access  Public
 router.post("/register", (req, res) => {
-  User.findOne({ email: req.user.email }).then(user => {
+  User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       return res.status(400).json({ email: "Email already exists" });
     } else {
@@ -27,14 +30,17 @@ router.post("/register", (req, res) => {
         d: "mm" // default
       });
       const newUser = new User({
-        name: req.user.name,
-        email: req.user.email,
+        name: req.body.name,
+        email: req.body.email,
         avatar,
-        password: req.user.password
+        password: req.body.password
       });
       bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err;
+          if (err) {
+            console.log(err);
+            throw err;
+          }
           newUser.password = hash;
           newUser
             .save()
@@ -43,6 +49,45 @@ router.post("/register", (req, res) => {
         });
       });
     }
+  });
+});
+
+// @route   POST api/users/register
+// @desc    Register a user
+// @access  Public
+router.post("login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+
+    // Compare password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        // User matched
+
+        //payload with id, name and avatar
+        const payload = { id: user.id, name: user.name, email: user.email };
+
+        // sign token
+        jwt.sign(
+          payload,
+          secretOrKey,
+          { expiresIn: 3600 * 24 * 3 },
+          (err, token) => {
+            res.send({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
+      } else {
+        res.status(400).json({ password: "Password Incorrect" });
+      }
+    });
   });
 });
 
