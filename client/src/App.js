4401006080c69d67c88652d46bd337676e3eaa52
ARYA-BHARDWAJ <aryabhardwaj009@gmail.com>
2019-03-30 16:35:04 +0530

    add login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from "react";
-import "./App.css";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Navbar from "./components/layout/navbar";
 import Footer from "./components/layout/footer";
 import Landing from "./components/layout/landing";
-//import "./bootstrap.min.css";
-//import "./all.min.css";
+import Login from "./components/auth/Login";
+// import "./bootstrap.min.css";
+// import "./all.min.css";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path = "/" component={Landing} />
+          <div className="container">
+            <Route exact path = "/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
